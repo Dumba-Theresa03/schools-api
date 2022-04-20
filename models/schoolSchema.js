b82e6b0bc235658a5e76d252a8f59a08020352ab
Dumba-Theresa03 <dumbatheresa3@gmail.com>
2022-04-20 14:57:09 +0100

    first commit

---
+++ models/schoolSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const schoolSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    level:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    numOfStuudents:{
+        type:Number,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    Adress:{
+        gps:String,
+        box:String
+    },
+    contact:{
+        phone:String,
+        fax:String,
+        email:String
+    },
+},{
+    timestamp:true
+}
+)
+
+const school = mongoose.model("school", schoolSchema)
+module.exports= school;
